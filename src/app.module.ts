99accf2279e167934fd0b15b69c5fd558ba720a0
Rimfis <dimitri.rim@yandex.ru>
2024-11-10 16:27:33 +0300

    feat: add base routers album

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TrackModule } from './track/track.module';
 import { ArtistModule } from './artist/artist.module';
+import { AlbumModule } from './album/album.module';
+import { AlbumModule } from './album/album.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [UsersModule, TrackModule, ArtistModule],
+  imports: [UsersModule, TrackModule, ArtistModule, AlbumModule],
 })
 export class AppModule {}
