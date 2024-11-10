c2c5d11535b002a708bdf6c98a7d52a5276064c2
Rimfis <dimitri.rim@yandex.ru>
2024-11-10 20:46:38 +0300

    feat: add base routers Favorites

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { UsersModule } from './users/users.module';
 import { TrackModule } from './track/track.module';
 import { ArtistModule } from './artist/artist.module';
 import { AlbumModule } from './album/album.module';
+import { FavsModule } from './favs/favs.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [UsersModule, TrackModule, ArtistModule, AlbumModule],
+  imports: [UsersModule, TrackModule, ArtistModule, AlbumModule, FavsModule],
 })
 export class AppModule {}
